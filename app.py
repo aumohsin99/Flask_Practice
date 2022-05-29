cd32a0bbffc4266f09136d773d919462aee55c53
Moshin <bsef19m031@pucit.edu.pk>
2022-05-29 21:57:27 +0500

    login partial

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask import render_template
+
+# from flask import
 
 app = Flask(__name__)
 
 
+@app.route('/welcome', methods=["POST"])
+def welcome():  # put application's code here
+    return 'Welcome, '
+
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def login_function():
+    # return render_template(index.html)
+    # return "Login Function tested!"
+    return render_template("index.html", user="Test user")
 
 
 if __name__ == '__main__':
